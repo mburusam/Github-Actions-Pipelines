9691865aef6a0fd2f5374a2a29b9c61b2bf6f456
Samuel Mburu <samuel.mburu@tracom.co.ke>
2022-05-01 23:59:14 +0300

    added files

---
+++ src/test/java/com/sam/cicdpipelinesgithubactions/CicdPipelinesGitHubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.cicdpipelinesgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPipelinesGitHubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
